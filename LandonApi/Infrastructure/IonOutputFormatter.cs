79c86e2b2f09943f4c69484a9d00b8cf01950f12
Zakariae EL ALAOUI EL BELGHITI <zakariaeelalaouielbelghiti@MacBook-Pro-de-Zakariae-2.local>
2018-11-01 09:32:30 +0100

    Update the media type

---
+++ LandonApi/Infrastructure/IonOutputFormatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+
+namespace LandonApi.Infrastructure
+{
+    public class IonOutputFormatter : TextOutputFormatter
+    {
+        private readonly JsonOutputFormatter _jsonOutputFormatter;
+
+        public IonOutputFormatter(JsonOutputFormatter jsonOutputFormatter)
+        {
+            _jsonOutputFormatter = jsonOutputFormatter ?? throw new ArgumentNullException(nameof(jsonOutputFormatter));
+
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/ion+json"));
+            SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        => _jsonOutputFormatter.WriteResponseBodyAsync(context, selectedEncoding);
+    }
+}
--- LandonApi/Startup.cs
+++ LandonApi/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LandonApi.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,7 +27,13 @@ namespace LandonApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(opt =>
+            {
+                var jsonFormatter = opt.OutputFormatters.OfType<JsonOutputFormatter>().Single();
+                opt.OutputFormatters.Remove(jsonFormatter);
+
+                opt.OutputFormatters.Add(new IonOutputFormatter(jsonFormatter));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddRouting(opt => opt.LowercaseUrls = true);
         }
