f24267608fb0635f16b3111e01c4e6436c280c2d
Zakariae EL ALAOUI EL BELGHITI <zakariaeelalaouielbelghiti@MacBook-Pro-de-Zakariae-2.local>
2018-11-01 09:19:40 +0100

    Create a root controller

---
+++ LandonApi/Controllers/RootController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace LandonApi.Controllers
+{
+    [Route("/")]
+    public class RootController : Controller
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+
+            return Ok(response);
+        }
+    }
+}
